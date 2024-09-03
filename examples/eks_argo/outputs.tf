3f0fa0d01739a0fef953b0828f41c2f007b26148
Rafael da Silva <rafaelds@amazon.com>
2024-09-03 03:11:46 -0300

    output eksclt kubeconfig-update command

--- examples/eks_argo/outputs.tf
+++ examples/eks_argo/outputs.tf
@@ -2,3 +2,8 @@ output "eks_cluster_name" {
   value       = local.cluster_name
   description = "EKS cluster name"
 }
+
+output "update_kubeconfig" {
+  value       = format("aws eks --region %s update-kubeconfig --name %s", data.aws_region.current.name, local.cluster_name)
+  description = "The command to update the kubconfig"
+}
